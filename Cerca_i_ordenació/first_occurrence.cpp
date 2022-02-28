aec87b67fcd7a31bea46b3effa0f7e33fa837d11
TuPapiLoko <baquedavid0@gmail.com>
2022-02-28 20:15:42 +0100

    second commit

--- Cerca_i_ordenació/first_occurrence.cpp
+++ Cerca_i_ordenació/first_occurrence.cpp
@@ -1,1 +1,25 @@
-jlñj
+#include <iostream> 
+#include <vector> 
+
+int first_occurrence(double x, const std::vector<double>& v) {
+    unsigned int pos, mid, left = 0, right = v.size();
+    
+    mid = v.size() / 2; 
+    
+    while(mid >= 0 and mid < v.size()) {
+        if(v[mid] == x) return int(mid); 
+        else{
+            if(v[mid] > x) {
+                right = mid;  
+                mid /= 2; 
+            }
+            else {
+                mid = (right + mid) / 2; 
+            }
+        }
+    }
+    return -1; 
+    
+}
+
+
